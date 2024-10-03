0acc32889a44035b89ed6abf56a246d3abbd76ec
Samaelish <semnd007@gmail.com>
2024-10-03 18:52:53 +0300

    created pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is the About page. Here is some info about me</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>Welcome to my Portfolio!</p>
+    </div>
+  )
+}
+
+export default Home
